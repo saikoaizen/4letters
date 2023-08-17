9bd0d3b31ca3aad31c5f14cdf23517450b8e00c1
FireWindCyclone <esh716@gmail.com>
2023-08-18 03:53:37 +0530

    Remove redundant image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()]
+  integrations: [react()],
 });
