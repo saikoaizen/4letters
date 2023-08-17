cb54586d66a55651e6cdd1789a7897eedf2ec6fb
FireWindCyclone <esh716@gmail.com>
2023-08-18 03:42:53 +0530

    remove nextjs

--- src/app/components/Title.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './Title.module.css'
-
-export default function Title() {
-  return <h1 className={styles.titleText}>4letters</h1>
-}
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import HomePage from './pages/HomePage'
-import GamePage from './pages/GamePage'
-import JoinPage from './pages/JoinPage'
-import RoomPage from './pages/RoomPage'
-
-export default function Home() {
-  return <HomePage />
-}
